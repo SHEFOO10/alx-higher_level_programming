e37a4029f4abeb964a3bf7c4a6c580a84ede01db
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-05 20:09:12 +0300

    explicit importing for functions

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-
 if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
     print('{} + {} = {}'.format(a, b, calc.add(a, b)))
     print('{} - {} = {}'.format(a, b, calc.sub(a, b)))
     print('{} * {} = {}'.format(a, b, calc.mul(a, b)))
