8415177b34d9323a528015b10973ecd46b640b4b
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-05 20:21:02 +0300

    3-infinite_add

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-import hidden_4
 
-[print(name) for name in dir(hidden_4) if not name.startswith('__')]
+if __name__ == '__main__':
+    import hidden_4
+    [print(name) for name in dir(hidden_4) if not name.startswith('__')]
