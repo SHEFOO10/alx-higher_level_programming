5e066d711d988a4a7d656640c08c392deb89aa8c
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-09 09:56:48 +0300

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix[0]:
-        for row in matrix:
-            for column in row:
-                print('{:d}'.format(column), end=' ')
-            print('')
+    for row in matrix:
+        for column in row:
+            print('{:d}'.format(column), end='')
+        print('')
