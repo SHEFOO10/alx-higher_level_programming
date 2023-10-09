c3d37cb0b26d95ffad4ac03858369f7899612f97
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-09 19:27:29 +0300

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+    max_number = 0
+    for item in my_list:
+        if item > max_number:
+            max_number = item
+        else:
+            continue
+    return max_number
