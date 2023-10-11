f5546b83ee751522c4e5f3f6352b93f024c75034
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-11 23:44:00 +0300

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in range(len(my_list)):
+        new_list.append(my_list[i] if my_list[i] != search else replace)
+    return new_list
