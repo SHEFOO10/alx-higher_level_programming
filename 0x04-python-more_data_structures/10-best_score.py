2f95f6f289db665f4169f923194e55ec0f729cee
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-12 01:48:03 +0300

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    max_score = sorted(
-        a_dictionary.items(),
-        key=lambda item: item[1], reverse=True)
-    return max_score[0][0]
+    max_score = max(list(a_dictionary.values()))
+    for item in a_dictionary.items():
+        if (item[1] == max_score):
+            return item[0]
