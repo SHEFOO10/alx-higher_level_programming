5839bfad34fa72f87932af397588c6514f8de181
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-12 01:37:17 +0300

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if (a_dictionary == None):
+        return None
+    max_score = sorted(a_dictionary.items(), key=lambda item:item[1], reverse=True)
+    return max_score[0][0]
