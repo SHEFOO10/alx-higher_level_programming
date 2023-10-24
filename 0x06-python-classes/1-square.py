ce8eac2faecc9fe16a383a617a8e24e18d94a48e
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-24 20:42:55 +0300

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    """Define Square"""
+    def __init__(self, size):
+        """
+            Initialize a square with specific size.
+
+            Args:
+                size (int): Size of the square.
+        """
+        self.__size = size
