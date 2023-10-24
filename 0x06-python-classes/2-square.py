e515057b551a17a8abe0c415c3de7c3e7707821a
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-24 20:58:05 +0300

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """ define a Square """
+    def __init__(self, size=0):
+        """
+            Initialize a square.
+
+            Args:
+                size (int): size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
