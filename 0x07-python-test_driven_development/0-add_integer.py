26cd790deaaf76572e5d75aa081e22f04e9a3177
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-10-27 13:37:11 +0200

    0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" 
+    add integer
+
+    function to calc sum of the two integers
+    """
+
+
+def add_integer(a, b=98):
+    """
+        add_integer : add integer to another.
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
