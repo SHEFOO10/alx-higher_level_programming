76645d211e284c39a90756a9c23971618531d906
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-11-01 21:24:58 +0200

    5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -9,6 +9,6 @@ def text_indentation(text):
     """ prints a text with 2 new lines if it's ends with . , ? and : """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    print(text.strip().replace(". ", ".\n")
-                      .replace("? ", "?\n")
-                      .replace(": ", ":\n"))
+    print(text.strip().replace(". ", ".\n\n")
+                      .replace("? ", "?\n\n")
+                      .replace(": ", ":\n\n"), end='')
