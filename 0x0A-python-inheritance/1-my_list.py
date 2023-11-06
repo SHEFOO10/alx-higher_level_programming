ba84e5d7bb56b4cbce4c32e20e954f88a923e71a
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-11-06 23:47:24 +0200

    1-my_list.py - fix pycodestyle

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,7 +3,7 @@
 
 
 class MyList(list):
-	""" class MyList that inherits from list """
-	def print_sorted(self):
-		""" prints the list, but sorted (ascending sort) """
-		print(sorted(self))
+    """ class MyList that inherits from list """
+    def print_sorted(self):
+        """ prints the list, but sorted (ascending sort) """
+        print(sorted(self))
