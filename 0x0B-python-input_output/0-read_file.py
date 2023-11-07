791a3085f2b767a56014086c6757f02cdf6ca8f4
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-11-07 23:35:02 +0200

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" 0. Read file """
+
+
+def read_file(filename=""):
+	"""
+		reads a text file (UTF8) and prints it to stdout.
+
+		Args:
+			filename (str): filename.
+	"""
+	with open(filename) as f:
+		print(f.read(), end='')
