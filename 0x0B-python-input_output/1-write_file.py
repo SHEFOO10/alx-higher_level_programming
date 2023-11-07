1105fc9de444d0a4999fc7b128dfd0bd54ba85fb
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-11-07 23:45:31 +0200

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 1. Write to a file """
+
+
+def write_file(filename="", text=""):
+    """
+        a function that writes a string to a text file (UTF8)
+        and returns the number of characters written
+    
+        Args:
+            filename (str): filename.
+            text (str): text.
+    """
+    with open(filename, 'w') as f:
+        wc = f.write(text)
+    return wc
