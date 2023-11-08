d950e5bfa8d9afaf35f1cc4195258d0461e1800e
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-11-09 01:39:00 +0200

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -11,7 +11,7 @@ def append_after(filename="", search_string="", new_string=""):
         content = file.readlines()
         i = 0
         for line in content:
-            if search_string in line:
+            if line.find(search_string) != -1:
                 content.insert(i + 1, new_string)
             i += 1
         file.seek(0)
