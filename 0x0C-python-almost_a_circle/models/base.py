8b552bcfcaaa0f6178d50d6887b3e0738cad8381
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-11-12 22:20:54 +0200

    Base Class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Base Class """
+
+
+class Base:
+    """ Base Class """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initiate new object"""
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
