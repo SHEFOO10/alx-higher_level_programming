68426b2e301cb556e9962799d4cceeca77b6bcda
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-11-14 01:11:21 +0200

    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Base Class """
 import json
+import os
 
 
 class Base:
@@ -20,3 +21,13 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is None:
+            return []
+        new_list = []
+        for obj in list_objs:
+            new_list.append(obj.to_dictionary())
+        with open(f'{cls.__name__}.json', 'w') as f:
+            f.write(Base.to_json_string(new_list))
