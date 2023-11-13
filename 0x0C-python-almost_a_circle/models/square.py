cecf7720f7f5081c36ece107f48986ca70b96b80
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-11-14 00:30:45 +0200

    models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,3 +19,21 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+            *args is the list of arguments - no-keyworded arguments
+
+            1st argument should be the id attribute
+            2nd argument should be the size attribute
+            3rd argument should be the x attribute
+            4th argument should be the y attribute
+        """
+        try:
+            self.id = args[0]
+            self.size = args[1]
+            self.x = args[2]
+            self.y = args[3]
+        except IndexError:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
