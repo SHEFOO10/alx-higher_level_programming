cdff7ceccd985f4017633be934ab94e86f852858
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-12-13 02:01:50 +0200

    7. First add

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- INSERT INTO table_name (column_name, (another_column_name, ...)) VALUES (first_column_value, (second_column_value, ...))
+-- inserts a new row in the table first_table
+INSERT INTO first_table (id, name) VALUES (89, 'Best School')
