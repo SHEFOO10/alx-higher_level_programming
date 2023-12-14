3331e1b8d536ebbf53fa04d43bb836d2abf33276
SHEFOO10 <445585913@g102019.moe.edu.eg>
2023-12-14 03:11:39 +0200

    fix order by to order with cities.id and remove USE command because it will be passed

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa.
-USE hbtn_0d_usa;
 SELECT cities.id, cities.name FROM states, cities
 WHERE states.name = 'California' AND states.id = cities.state_id
-ORDER BY id ASC;
+ORDER BY cities.id ASC;
