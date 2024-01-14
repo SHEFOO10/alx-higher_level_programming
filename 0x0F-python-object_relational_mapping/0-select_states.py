f02be5ac8a45237c1841dc1b8b8f2e9407962829
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-01-14 20:35:11 +0200

    pycodestyle errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,18 +3,21 @@
 import MySQLdb
 import sys
 
+
 def list_states():
     """ list states from database """
+
     db = MySQLdb.connect(host='localhost',
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3],
-                        port=3306)
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
     cursor = db.cursor()
 
     cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
     for record in cursor.fetchall():
         print(record)
+
 
 if __name__ == '__main__':
     list_states()
