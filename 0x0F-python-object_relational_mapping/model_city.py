d8ac95bfb8fa3c779866b0a6919cc4fedd9529cb
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-01-15 03:09:10 +0200

    fixed

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,6 +2,7 @@
 """ City Model """
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import State, Base
+from sqlalchemy.orm import relationship
 
 class City(Base):
     """ City Model Class """
@@ -10,3 +11,4 @@ class City(Base):
                 autoincrement=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey(State.id), nullable=False)
+    State = relationship(State)
