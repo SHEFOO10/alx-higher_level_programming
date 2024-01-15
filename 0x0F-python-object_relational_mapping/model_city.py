dd52d07ed19f3931480e2371e10a553892176304
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-01-15 02:57:15 +0200

    .....:

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 """ City Model """
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from model_state import State
-from sqlalchemy.orm import relationship
-
-Base = declarative_base()
-
+from model_state import State, Base
 
 class City(Base):
     """ City Model Class """
