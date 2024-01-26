d933c3e3e64071332f48f9fe14de905d2043aee0
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-01-25 18:03:56 -0800

    6. Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,27 +3,27 @@
 
 
 def find_peak(list_of_integers):
-	""" finds a peak in a list of unsorted integers. """
-	length = len(list_of_integers)
-	if (length == 0):
-		return None
-	if (length == 1):
-		return list_of_integers[0]
-	return find_peak_recursive(list_of_integers, 0, length - 1)
+    """ finds a peak in a list of unsorted integers. """
+    length = len(list_of_integers)
+    if (length == 0):
+        return None
+    if (length == 1):
+        return list_of_integers[0]
+    return find_peak_recursive(list_of_integers, 0, length - 1)
 
 
 def find_peak_recursive(integers, low, high):
-	""" recursive find_peak """
+    """ recursive find_peak """
 
-	if (low == high):
-		return integers[low]
+    if (low == high):
+        return integers[low]
 
-	mid = (low + high) // 2
+    mid = (low + high) // 2
 
-	if (integers[mid] >= integers[mid + 1] and integers[mid] >= integers[mid - 1]):
-		return integers[mid]
-	elif (integers[mid] < integers[mid + 1]):
-		return find_peak_recursive(integers, mid + 1, high)
-	else:
-		return find_peak_recursive(integers, low, mid - 1)
-
+    if (integers[mid] >= integers[mid + 1] and
+        integers[mid] >= integers[mid - 1]):
+        return integers[mid]
+    elif (integers[mid] < integers[mid + 1]):
+        return find_peak_recursive(integers, mid + 1, high)
+    else:
+        return find_peak_recursive(integers, low, mid - 1)
