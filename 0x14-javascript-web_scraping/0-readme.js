46c9c371ebc4775aad650fe34206c2963d8766d4
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-02-20 21:15:17 +0200

    retype utf8 -> utf-8

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const process = require('process');
 
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (data) {
     console.log(data);
   } else {
