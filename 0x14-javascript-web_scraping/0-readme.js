aaaf7da83c930c2fe70e4fba5e8e6a29c6fd23e4
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-02-20 21:29:02 +0200

    handle no file path is passed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+const filename = process.argv[2] ? process.argv[2] : '' 
+fs.readFile(filename, 'utf8', (err, data) => {
   if (data) {
     console.log(data);
   } else {
