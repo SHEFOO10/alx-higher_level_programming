d1e1f440d3d26adf2c9097fd2908503f63b0bdd2
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-02-20 21:32:24 +0200

    replace console.log with console.error for displaying errors

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const filename = process.argv[2] ? process.argv[2] : '' 
-fs.readFile(filename, 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (data) {
     console.log(data);
   } else {
-    console.log(err);
+    console.error(err);
   }
 });
