651b6d9a72203c0914bdd954a2c95321faa7e0c5
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-02-20 21:37:11 +0200

    replace readFile with readFileSync

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
-  if (data) {
-    console.log(data);
-  } else {
-    console.error(err);
-  }
-});
+try {
+  const data = fs.readFileSync(process.argv[2], 'utf8');
+  console.log(data);
+} catch (err) {
+  console.error(err);
+}
