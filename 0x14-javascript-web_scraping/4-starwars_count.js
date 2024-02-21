90638250f0d0a02343e7ba9126e3869f1c667630
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-02-21 21:13:28 +0200

    4. Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -9,7 +9,7 @@ request.get(process.argv[2], (err, response, body) => {
   } else {
     const res = JSON.parse(body).results;
     for (const movie of res) {
-      if (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18') || movie.characters.includes('http://swapi.co/api/people/18/')) { numberOfMovies++; }
+      if (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/') || movie.characters.includes('http://swapi.co/api/people/18/')) { numberOfMovies++; }
     }
     console.log(numberOfMovies);
   }
