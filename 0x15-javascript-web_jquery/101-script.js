bbfbd13b3b307c13db58e0c5e992021f308fc70e
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-02-28 20:47:52 +0200

    11. List, add, remove

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  $('DIV#add_item').on('click', () => {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('DIV#remove_item').on('click', () => {
+    $('UL.my_list').children().last().remove();
+  });
+  $('DIV#clear_list').on('click', () => {
+    $('UL.my_list').empty();
+  });
+});
